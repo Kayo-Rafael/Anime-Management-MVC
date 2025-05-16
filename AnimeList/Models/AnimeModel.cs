d260f24dff31d5d655274201d4fae11f70f8b405
Kayo Rafael <kayorafaelpj@gmail.com>
2025-05-16 15:18:17 -0300

    Validações de informações da Model criada

--- AnimeList/Models/AnimeModel.cs
+++ AnimeList/Models/AnimeModel.cs
@@ -1,11 +1,17 @@
-﻿namespace AnimeList.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimeList.Models;
 
 public class AnimeModel
 {
     public int Id { get; set; }
+    [Required(ErrorMessage ="O Nome de usuario é Obrigatorio")]
     public string Usuario { get; set; } = string.Empty;
+    [Required(ErrorMessage = "O Nome do Anime é Obrigatorio")]
     public string NomeAnime { get; set; } = string.Empty;
     public string Genero { get; set; } = string.Empty;
+    [Required(ErrorMessage = "O Nome de Criador é Obrigatorio")]
     public string Criador { get; set; } = string.Empty;
+    [Required(ErrorMessage = "A Data de lançamento é Obrigatoria")]
     public int AnoLancamento { get; set; }
 }
